d148520bf178cdc3e40c16980297c9cec1237a05
koppunoorisrikar <koppunoorisrikar@gmail.com>
2025-07-26 16:52:48 +0530

    added front end deploy link to cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://frontend-bagify.onrender.com"],
     credentials: true,
   })
 );
